706ec26cb1fe048898f6676b0272b01d7248052a
rainbowpython <39071822+rainbowpython@users.noreply.github.com>
2023-06-05 05:17:11 +0000

    arbu

--- Grapher.java
+++ Grapher.java
@@ -6,41 +6,23 @@ public class Grapher {
   private String xSide;
 
   private String eqaution;
-  private int yIntercept;
-  private int xCoefficent;
-  //private ScriptEngineManager mgr = new ScriptEngineManager(null);
-  //private ScriptEngine engine = mgr.getEngineByName("JavaScript");
+  private double yIntercept;
+  private double slope;
   private ArrayList<Point> graph = new ArrayList<Point>();
   private Display display;
 
-  public Grapher(String newEquation) {
+  public Grapher(double newSlope, double newYIntercept) {
     new ScriptEngineManager().getEngineFactories();
-    eqaution = newEquation;
-    createXSide();
+    slope = newSlope;
+    yIntercept = newYIntercept;
     calculate();
 
-  }
-
-  private void createXSide() {
-    xSide = eqaution.substring(eqaution.indexOf("=") + 1);
-    System.out.println(xSide);
-    xCoefficent = Integer.parseInt(eqaution.substring(eqaution.indexOf("x") - 1, eqaution.indexOf("x")));
   }
 
   private void calculate() {
     for (int y = 10; y >= 0; y--) {
       for (int x = 0; x < 10; x++) {
-        String input = "" + (x * xCoefficent) + eqaution.substring(eqaution.indexOf(x) + 1);
-        try {
-          if (Integer.parseInt(engine.eval(input).toString()) == y) {
-            graph.add(new Point(x, y));
-          }
-        } catch (NumberFormatException | ScriptException e) {
-          if (e instanceof NullPointerException) {
-            System.out.println("ERROR: incorrect format");
-          }
-          System.out.println(graph);
-        }
+        
       }
     }
     System.out.println(graph);
